44cb56bc4b40fe68aa36795c6d3743c813e95859
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-11-13 23:03:34 +0530

    Udate systemjs-imports

--- src/react-mf-root-config.js
+++ src/react-mf-root-config.js
@@ -1,30 +1,20 @@
-import {
-  constructRoutes,
-  constructApplications,
-  constructLayoutEngine,
-} from "single-spa-layout";
 import { registerApplication, start } from "single-spa";
 
-const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
-  loaders: {
-    topNav: "<h1>Loading topnav</h1>",
-  },
-  errors: {
-    topNav: "<h1>Failed to load topnav</h1>",
-  },
-});
-const applications = constructApplications({
-  routes,
-  loadApp: ({ name }) => System.import(name),
-});
-// Delay starting the layout engine until the styleguide CSS is loaded
-const layoutEngine = constructLayoutEngine({
-  routes,
-  applications,
-  active: false,
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: "@react-mf/navbar",
+  app: () => System.import("@react-mf/navbar"),
+  activeWhen: ["/"],
 });
 
-applications.forEach(registerApplication);
-
-layoutEngine.activate();
-start();
+start({
+  urlRerouteOnly: true,
+});
